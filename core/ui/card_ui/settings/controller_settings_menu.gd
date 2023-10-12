e8718b63a1c975e3d27dbf981349e70cbaac5804
Atapi <34801996+Sterophonick@users.noreply.github.com>
2023-10-12 14:06:56 -0600

    fill out a bit of meat on the script

--- core/ui/card_ui/settings/controller_settings_menu.gd
+++ core/ui/card_ui/settings/controller_settings_menu.gd
@@ -1,11 +1,15 @@
-extends Node
+extends Control
 
+var SettingsManager := load("res://core/global/settings_manager.tres") as SettingsManager
+@onready var allow_guideless_toggle = $%AllowGuidelessToggle
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	bool allow_guideless := SettingsManager.get_value("controllers", "allow_guideless_controllers", false) as bool
+	allow_guideless_toggle.value = allow_guideless
+	allow_guideless_toggle.value_changed.connect(_on_guideless_changed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_guideless_changed(value: bool) -> void:
+	SettingsManager.set_value("controllers", "allow_guideless_controllers", value)
